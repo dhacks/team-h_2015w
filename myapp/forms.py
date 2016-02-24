7261d4ffc279c930ed01e3392f82798467a205ba
ishibashi <kyayg@users.noreply.github.com>
2016-02-24 16:33:38 +0900

    post system.

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,6 @@
 from myapp import app
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 
 
@@ -16,3 +16,8 @@ class Login(Form):
     email = StringField(validators=[Required(),Email()])
     pswd = PasswordField()
 
+class Post_Form(Form):
+    title = StringField('title',validators=[Required()])
+    tag =  StringField('tag')
+    body = TextAreaField('body',validators=[Required()])
+    submit = SubmitField('submit')
