2521f259f4f2e986cfbabd12e10bfe6b0d22edf2
Eshin Kunishima <omochi@luna.miko.im>
2016-02-26 10:53:10 +0900

    refactored forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,34 +1,32 @@
-from myapp import app
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, TextAreaField, SubmitField
-from wtforms.validators import Required, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
-class Signup(Form):
-    username = StringField('Name', validators=[Required()])
-    email = StringField('Email', validators=[Required(),Email()])
-    pswd1 = PasswordField('Create a password',)
-    pswd2 = PasswordField('Confirm your password', validators=[EqualTo(pswd1)])
+class SignupForm(Form):
+    username = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Create a password', )
+    password_confirm = PasswordField('Confirm your password', validators=[EqualTo(password)])
 
 
-class Login(Form):
-
-    email = StringField(validators=[Required(),Email()])
-    pswd = PasswordField()
-    submit = SubmitField('submit')
-
-class Post_Form(Form):
-    title = StringField('title',validators=[Required()])
-    tag =  StringField('tag')
-    body = TextAreaField('body',validators=[Required()])
+class LoginForm(Form):
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField()
     submit = SubmitField('submit')
 
 
-class Comment_Form(Form):
+class PostForm(Form):
+    title = StringField('title', validators=[DataRequired()])
+    tag = StringField('tag')
+    body = TextAreaField('body', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
-    cbody = TextAreaField('comment body', validators=[Required()])
 
-class Search_Form(Form):
+class CommentForm(Form):
+    body = TextAreaField('comment body', validators=[DataRequired()])
 
+
+class SearchForm(Form):
     tag = StringField('tag')
     submit = SubmitField('検索')
